0ca10c3ed4cda285c1cf0959998defbaa91d3575
Quantum Snowball <quantum.snowball@gmail.com>
2021-02-09 18:39:25 +0800

    cap_list

---
+++ cap_list.py
@@ -0,0 +1,14 @@
+import json
+import requests
+import pandas as pd
+
+url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+
+resp = requests.get(url)
+data = json.loads(resp.content)
+df = pd.DataFrame(data)
+
+selected = df[['name','symbol','market_cap','price_change_percentage_24h',]]
+selected.symbol = selected.symbol.apply(str.upper)
+print(selected.head(30))
+
